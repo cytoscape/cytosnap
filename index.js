d22ddacb2a21cc22813f5277d00175c99551a744
Max Franz <maxkfranz@gmail.com>
2016-06-02 10:46:48 -0400

    add scaffolding based on #1

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// TODO entry point of lib...
+var phantom = require('phantom');
+var cytoscape = require('cytoscape');
+var bluebird = require('bluebird');
+var lodash = require('lodash');
+
+var Cytosnap = function( opts ){
+  if( !(this instanceof Cytosnap) ){
+    return new Cytosnap( opts );
+  }
+
+  this.options = _.assign( {
+    // defaults
+    autostart: true
+  }, opts );
+
+  if( this.options.autostart ){
+    this.start();
+  }
+};
+
+var csp = Cytosnap.prototype;
+
+csp.start = function(){
+  // TODO
+};
+
+csp.stop = function(){
+  // TODO
+};
+
+csp.snap = function(){
+  // TODO
+};
+
+module.exports = Cytosnap;
