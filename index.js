847813963cebb4414396369cd1f58c2f33075b7f
Max Franz <maxkfranz@gmail.com>
2016-06-02 13:17:35 -0400

    adds basic phantom integration

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 var phantom = require('phantom');
 var cytoscape = require('cytoscape');
-var bluebird = require('bluebird');
-var lodash = require('lodash');
+var Promise = require('bluebird');
+var _ = require('lodash');
+var safeCall = function( fn, args ){
+  if( fn ){
+    args = args || [];
+
+    fn.apply( fn, args );
+  }
+};
 
 var Cytosnap = function( opts ){
   if( !(this instanceof Cytosnap) ){
@@ -10,26 +17,65 @@ var Cytosnap = function( opts ){
 
   this.options = _.assign( {
     // defaults
-    autostart: true
+
   }, opts );
 
-  if( this.options.autostart ){
-    this.start();
-  }
+  this.running = false;
 };
 
-var csp = Cytosnap.prototype;
+var proto = Cytosnap.prototype;
 
-csp.start = function(){
-  // TODO
+proto.start = function( next ){
+  var snap = this;
+
+  return Promise.try(function(){
+    return phantom.create();
+  }).then(function( phantomInstance ){
+    snap.phantom = phantomInstance;
+
+    snap.running = true;
+  }).then(function(){
+    safeCall( next );
+  });
 };
 
-csp.stop = function(){
-  // TODO
+proto.stop = function( next ){
+  var snap = this;
+
+  return Promise.try(function(){
+    snap.phantom.exit();
+  }).then(function(){
+    snap.running = false;
+  }).then(function(){
+    safeCall( next );
+  });
 };
 
-csp.snap = function(){
-  // TODO
+proto.shot = function( opts, next ){
+  var snap = this;
+  var page;
+
+  opts = _.assign( {
+    // defaults
+  }, opts );
+
+  return Promise.try(function(){
+    return snap.phantom.createPage();
+  }).then(function( phantomPage ){
+    page = phantomPage;
+
+    return page.open('./phantom/index.html');
+  }).then(function( status ){
+    var img = null; // TODO generate image
+
+    page.close();
+
+    return img;
+  }).then(function( img ){
+    safeCall( next, [ img ] );
+
+    return img;
+  });
 };
 
 module.exports = Cytosnap;
--- test/service.js
+++ test/service.js
@@ -1,21 +1,26 @@
 var chai = require('chai');
 var expect = chai.expect;
+var cytosnap = require('../');
+var Promise = require('bluebird');
 
 describe('Service', function(){
-  beforeEach(function(){
-    // setup
+  var snap;
+
+  before(function( done ){
+    snap = cytosnap();
+
+    snap.start().then( done );
   });
 
-  afterEach(function(){
-    // teardown
+  it('should run async in bg', function( done ){
+    Promise.delay( 1000 ).then(function(){
+      expect( snap.running ).to.be.true;
+    }).then( done );
   });
 
-  it('should run async in bg', function( next ){
-    expect( true ).to.be.true; // TODO
-    next();
-  });
-
-  it('should stop when requested', function(){
-    expect( true ).to.be.true; // TODO
+  it('should stop when requested', function( done ){
+    snap.stop().then(function(){
+      expect( snap.running ).to.be.false;
+    }).then( done );
   });
 });
